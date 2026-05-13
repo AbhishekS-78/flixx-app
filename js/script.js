34651a0a94b67d8efe340cea10fef0c932fb9929
Abhishek S <thisisabhishek.78@gmail.com>
2026-05-13 22:20:27 +0530

    Display Movie Details

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ async function displayPopularMovies() {
   results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('card');
+
     div.innerHTML = `
       <a href="movie-details.html?id=${movie.id}">
         ${
@@ -51,6 +52,7 @@ async function displayPopularShows() {
   results.forEach((show) => {
     const div = document.createElement('div');
     div.classList.add('card');
+
     div.innerHTML = `
       <a href="tv-details.html?id=${show.id}">
         ${
@@ -81,6 +83,79 @@ async function displayPopularShows() {
 
     document.querySelector('#popular-shows').appendChild(div);
   });
+}
+
+// Display movie details
+async function displayMovieDetails() {
+  // Get the ID (?=387283)
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+        ${
+          movie.poster_path
+            ? `
+          <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />
+          `
+            : `
+          <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />
+          `
+        }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        
+        <p>${movie.overview}</p>
+        
+        <h5>Genres</h5>
+        <ul class="list-group">${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}</ul>
+        
+        <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+
+        <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies
+        .map(
+          (company) => `<span>
+        ${company.name}
+        </span>`,
+        )
+        .join('')}</div>
+    </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
 }
 
 // Fetch data from TMDB API
@@ -123,6 +198,10 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Page router: required to add functions that are specific to each page
 
 // Init App
@@ -136,7 +215,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV details');
