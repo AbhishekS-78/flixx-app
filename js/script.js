5814c7118c6423cd0383ae73666b46e8fc7c9ed0
Abhishek S <thisisabhishek.78@gmail.com>
2026-05-13 18:12:06 +0530

    Display popular movies on the homepage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,61 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  // An array of results, destructuring
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        ${
+          // Check if the image exists to display no-image if its doesn't exist, ternary op
+          movie.poster_path
+            ? `
+          <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />
+          `
+            : `
+          <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />
+          `
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'c375dcb01b2b4ddcccae697b257b74f4';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  // Through the documentation of API
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll('.nav-link');
@@ -19,7 +74,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
